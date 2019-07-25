e7668ed9491f125e92b727b941fea3007bdc4bf8
alex <cyy0523xc@gmail.com>
2019-07-25 21:06:43 +0800

    train

--- docker-start.sh
+++ docker-start.sh
@@ -6,6 +6,15 @@
 # Author: alex
 # Created Time: 2019年07月25日 星期四 17时51分49秒
 
+if [ ! -f docker-start.sh ]; then
+    echo '只允许在项目根目录运行'
+    exit 1
+fi
+
+if [ ! -d checkpoint ]; then
+    mkdir checkpoint
+fi
+
 # 语料库地址
 corpus=/home/ibbd/corpus/
 
--- train.py
+++ train.py
@@ -7,11 +7,11 @@ from keras.callbacks import ModelCheckpoint
 # 0.准备训练所需数据------------------------------
 data_args = data_hparams()
 data_args.data_type = 'train'
-data_args.data_path = '../dataset/'
+data_args.data_path = '/corpus/'
 data_args.thchs30 = True
-data_args.aishell = True
-data_args.prime = True
-data_args.stcmd = True
+data_args.aishell = False
+data_args.prime = False
+data_args.stcmd = False
 data_args.batch_size = 4
 data_args.data_length = 10
 # data_args.data_length = None
@@ -21,22 +21,26 @@ train_data = get_data(data_args)
 # 0.准备验证所需数据------------------------------
 data_args = data_hparams()
 data_args.data_type = 'dev'
-data_args.data_path = '../dataset/'
+data_args.data_path = '/corpus/'
 data_args.thchs30 = True
-data_args.aishell = True
+data_args.aishell = False
 data_args.prime = False
 data_args.stcmd = False
-data_args.batch_size = 4
+data_args.batch_size = 20
 # data_args.data_length = None
 data_args.data_length = 10
 data_args.shuffle = True
 dev_data = get_data(data_args)
 
 # 1.声学模型训练-----------------------------------
+print("*"*80)
+print("训练声学模型...")
+print("*"*80)
 from model_speech.cnn_ctc import Am, am_hparams
 am_args = am_hparams()
 am_args.vocab_size = len(train_data.am_vocab)
-am_args.gpu_nums = 1
+#am_args.gpu_nums = 1
+am_args.gpus = 2
 am_args.lr = 0.0008
 am_args.is_training = True
 am = Am(am_args)
@@ -45,12 +49,14 @@ if os.path.exists('logs_am/model.h5'):
     print('load acoustic model...')
     am.ctc_model.load_weights('logs_am/model.h5')
 
-epochs = 10
+epochs = 20
 batch_num = len(train_data.wav_lst) // train_data.batch_size
 
 # checkpoint
 ckpt = "model_{epoch:02d}-{val_acc:.2f}.hdf5"
-checkpoint = ModelCheckpoint(os.path.join('./checkpoint', ckpt), monitor='val_loss', save_weights_only=False, verbose=1, save_best_only=True)
+checkpoint = ModelCheckpoint(os.path.join('./checkpoint', ckpt),
+                             monitor='val_acc', save_weights_only=False,
+                             verbose=1, save_best_only=True)
 
 #
 # for k in range(epochs):
@@ -67,6 +73,9 @@ am.ctc_model.save_weights('logs_am/model.h5')
 
 
 # 2.语言模型训练-------------------------------------------
+print("*"*80)
+print("训练语言模型...")
+print("*"*80)
 from model_language.transformer import Lm, lm_hparams
 lm_args = lm_hparams()
 lm_args.num_heads = 8
